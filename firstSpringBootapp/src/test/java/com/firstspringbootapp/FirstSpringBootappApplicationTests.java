f4cfb65bf446134fa6fd622723e72d85ff9f8748
abhiram7352 <abhiramkumar8780@gmail.com>
2025-02-21 21:40:49 +0530

    [Abhiram] Added Resources to main 🎉

---
+++ firstSpringBootapp/src/test/java/com/firstspringbootapp/FirstSpringBootappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstspringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
